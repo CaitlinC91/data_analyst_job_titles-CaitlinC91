dac85fa529357f5a3a7b965a55bf2ce12f860f6f
Caitlin Craig <caitlincraig1016@gmail.com>
2023-02-07 19:55:56 -0600

    7done

--- scripts/data_analyst_jobs.sql
+++ scripts/data_analyst_jobs.sql
@@ -40,13 +40,27 @@ WHERE location = 'TN' AND star_rating >4;
 SELECT review_count
 FROM data_analyst_jobs
 WHERE review_count
-BETWEEN 500 AND 1000
+BETWEEN 500 AND 1000;
 
 --151
 
 -- Show the average star rating for companies in each state. The output should show the state as state and the average rating for the state as avg_rating. Which state shows the highest average rating?
 
+SELECT location AS state, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+GROUP BY location
+ORDER BY avg_rating DESC;
+
+-- NE
+
+
 -- Select unique job titles from the data_analyst_jobs table. How many are there?
+
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs;
+
+--881
+
 
 -- How many unique job titles are there for California companies?
 
